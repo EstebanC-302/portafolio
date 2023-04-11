f862e9b53d4b265ac84f6b85c55da480440a0501
estebanC-302 <estebanrcc69@gmail.com>
2023-04-11 16:50:52 -0300

    Se agrega el CV actualizado

--- src/sectionDescription/SectionDescription.js
+++ src/sectionDescription/SectionDescription.js
@@ -18,13 +18,13 @@ function SectionDescription () {
                 </div>
                 {/* <!-- About Section Content--> */}
                 <div className="row">
-                    <div className="col-lg-4 ml-auto"><p class="lead">Soy venezolano tengo 21 años de edad me considero una persona  proactiva, responsable, honesta y puntual, me encanta la progrmacion. Cada dia me mantengo en continuo conocimiento y actualizado con las actualizaciones que les realizan a las tecnologias que utilizo día a día para el desarrollo frontend.</p></div>
+                    <div className="col-lg-4 ml-auto"><p class="lead">Soy una persona proactiva, responsable, honesta, puntual y con buenas relaciones interpersonales. Siempre tengo la mejor disposición para la realización de mis labores. Busco un puesto de trabajo desafiante. Cuento con ciertos  conocimiento en diferentes tecnologías como HTML5, Css3, Js, React, BOOTSTRAP, tailwind y Node js entre otros. Me encuentro en la búsqueda de la oportunidad para ingresar en el área del desarrollo front-end, para poner en practica los conocimientos adquiridos hasta ahora.</p></div>
                     <div className="col-lg-4 mr-auto"><p class="lead">Aparte de dedicarle ciertas horas a la programacion , tambien me gusta realizar actividades fisicas, recreativas  y culinarias.</p></div>
                 </div>
                 {/* <!-- About Section Button--> */}
                 <div className="text-center mt-4">
                         <button>
-                            <a href="https://drive.google.com/file/d/1X8_n9ENh7t_YaCx8xNLl3E6NqNY6NB-0/view?usp=sharing" target="blank_" className="btn btn-xl btn-outline-light">
+                            <a href="https://drive.google.com/file/d/1MS7OKFpkgwp9rmjQuMN_xS9yzqfZKPn1/view?usp=share_link" target="blank_" className="btn btn-xl btn-outline-light">
                                 <i className="fas fa-download mr-2"></i>
                                 Curriculum
                             </a>
